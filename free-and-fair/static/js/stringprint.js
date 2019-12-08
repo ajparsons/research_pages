24421ce1ab1e1a397c222fc7d70c857343f591c0
Alex <alex@alexparsons.co.uk>
2019-12-08 18:31:09 +0000

    updated with new tweaks to js and javascript

--- free-and-fair/static/js/stringprint.js
+++ free-and-fair/static/js/stringprint.js
@@ -571,14 +571,14 @@ function citeBox(dialog_title,link, para_id, para_tag){
         ele_id = para_id    ;
     };
 	
-	box_content = 'Link: <a href="' + link+ '">' + title + current_section_name + ', ' + ele_name + ' '+ ele_id + '</a>'
+	box_content = 'Link : <a href="' + link+ '">' + title + current_section_name + ', ' + ele_name + ' '+ ele_id + '</a>'
 	if (include_citation == "true"){
     box_content = box_content + '<br><br>Cite As:<br><span class="cite"> ' + cite_author + ' (' + year + '), <i>'+ full_title + '</i>. [online] , ' + org + '. '+ ele_name + ': ' + ele_id + ', Available at: '+ link + ' [Accessed ' + getNow() + ']</span>'
-	}
-    swal({
-    title: dialog_title,    
-    text: box_content,
-    html: true,
+	};
+    box_content = "<h4><b>" + dialog_title + "</b></h4>" + box_content
+    Swal.fire({
+    icon: 'success',
+    html: box_content,
     confirmButtonText: "OK" });
 	
 	send_ga("Generated Link", link)
